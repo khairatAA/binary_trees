1998e03843386f0b5a26eecb930188f55eaa1fe2
Khairat Adesina <khairatadesina01@gmail.com>
2023-11-02 12:02:35 +0100

    measures the size of a binary tree

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - measures the size of a binary tree
+ * @tree: pointer to the root node of the tree to measure the size
+ * Return: the height or 0
+ */
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t left;
+	size_t right;
+
+	if (tree == NULL)
+		return (0);
+
+	left = binary_tree_size(tree->left);
+	right = binary_tree_size(tree->right);;
+
+	return (left + right + 1);
+}
